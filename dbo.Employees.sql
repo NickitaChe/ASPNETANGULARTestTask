3512d2575cd5d8541ba5f995952052b22630124b
Chegodaev Nickita <95642647+NickitaChe@users.noreply.github.com>
2022-11-12 20:02:33 +0300

    Создание Таблицы

---
+++ dbo.Employees.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeID]    INT            IDENTITY (1, 1) NOT NULL,
+    [EmployeeName]  NVARCHAR (MAX) NOT NULL,
+    [Department]    NVARCHAR (MAX) NULL,
+    [DateOfJoining] NVARCHAR (MAX) NULL,
+    [Salary]        INT            NOT NULL,
+    [DateOfBirth] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
+);
+
